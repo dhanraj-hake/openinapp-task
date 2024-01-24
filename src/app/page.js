9de2b01ab75025135b5e96891641ace94a807fc8
Dhanraj Hake <113380195+dhanraj-hake@users.noreply.github.com>
2024-01-24 12:18:41 +0530

    Updating Links

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 import 'bootstrap/dist/css/bootstrap.css';
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      
+    <main >
+      <Link href="/login">Login</Link>
+      <br />
+      <Link href="/dashboard">Dashboard</Link>
     </main>
   );
 }
